06c5b883362496666b09a4604ca4f9e1eaaa68a9
Nam Cao <namcao26102000@gmail.com>
2020-11-20 08:39:35 +0700

    Tao App va duyet mang tu file data

---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Toi se yeu em mai mai  '
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: './images/p2.jpg',
+            price: 120,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Xem truoc khi uong'
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: './images/p3.jpg',
+            price: 220,
+            brand: 'Lascote',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'Ao hang con ca sau'
+        },
+        {
+            _id: '4',
+            name: 'Lacoste Free Shirt',
+            category: 'Pants',
+            image: './images/p4.jpg',
+            price: 110,
+            brand: 'Lascote',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'Ao hang con ca sau'
+        },
+        {
+            _id: '5',
+            name: 'Puma slimz pant',
+            category: 'Pants',
+            image: './images/p5.jpg',
+            price: 68,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Ao hang con ca sau'
+        },
+        {
+            _id: '6',
+            name: 'Adidas Fit Pant',
+            category: 'Pants',
+            image: './images/p6.jpg',
+            price: 117,
+            brand: 'Adidas',
+            rating: 3,
+            numReviews: 15,
+            description: 'Ao hang con ca sau'
+        }
+    ]
+}
+
+export default data;

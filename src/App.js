0c3b8783c3188e8a6f4bd61b41b3bc871a733b1f
Nam Cao <namcao26102000@gmail.com>
2020-11-20 13:41:38 +0700

    Tao component router and render products, product detail

--- src/App.js
+++ src/App.js
@@ -1,61 +1,32 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="/">Amazona</a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
-        <main>
-          <div className="row center">
-            {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img className="img medium" src={product.image} alt="product" />
-                </a>
-
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">
-                    <span>${product.price}</span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </main>
-        <footer className="row center">
-          All right reserve
+    <BrowserRouter>
+      <div className="App">
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="/">Amazona</a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+            </div>
+          </header>
+          <main>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
+          </main>
+          <footer className="row center">
+            All right reserve
         </footer>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
